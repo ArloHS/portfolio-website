845aec445fc54d8821f5913f40a2ae2418e8d608
ArloHS <arlo.steyn@gmail.com>
2026-08-24 15:05:08 +0200

    chnaged next js version

--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Ensure proper handling of file paths on Windows
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      }
-    }
-    return config
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
